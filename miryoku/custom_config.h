4cce6e3db1818041b1bcabb951adcc5f9576e99a
Sean <seancatchpole@users.noreply.github.com>
2023-05-12 10:15:30 -0400

    Revert "Update custom_config.h"
    This reverts commit f972c59b9a7873f71294bf856ad0d93b6d6cbe13.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,5 +1,4 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-&nice_view_spi {
-    cs-gpios = <&pro_micro 0 GPIO_ACTIVE_HIGH>;
-};
+
+cs-gpios = <&pro_micro 0 GPIO_ACTIVE_HIGH>;
